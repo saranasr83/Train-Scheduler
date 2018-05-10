271ae78e895fbb8ff8bd048f66914023a31ebde4
saranasr83 <saranasr83@gmail.comgit config --global user.email saranasr83@gmail.comgit config --global user.email saranasr83@gmail.com>
2018-05-10 00:27:59 -0700

    looking around and check the accuracy of everything using console.log

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,19 +12,20 @@ $(document).ready(function () {
     };
     firebase.initializeApp(config);
     //a path to the database, it stores all the input data
-    var database= firebase.database()
-    
-    //take all the form data and push it to firebase database
-    $("#myform").submit(function(event){
+    var database = firebase.database()
+
+    //take all the form data and push it to firebase database(button for adding train info)
+    $("#myform").submit(function (event) {
         event.preventDefault();
         //console.log("works");
-        //the value of inputs
-        var trainName = $("#train-name").val()
-        var destination = $("#destination").val()
-        var firstTrainTime = $("#first-train-time").val()
-        var frequency = $("#frequency").val()
+        //the value of user inputs
+        var trainName = $("#train-name").val().trim()
+        var destination = $("#destination").val().trim()
+        var firstTrainTime = $("#first-train-time").val().trim()
+        var frequency = $("#frequency").val().trim()
         //console.log(trainName)
-        //push our data to firebase every time the form gets submited with unoque key
+        //holding the train info/data in local object
+        //push(upload) our data to firebase every time the form gets submited with unique key
         database.ref().push({
             trainName: trainName,
             destination: destination,
@@ -34,8 +35,9 @@ $(document).ready(function () {
     })
     //take the data back from firabase and loop through it
     //console.log(moment().format("hh:mm a"));
-    //chilsanapshot,snaphot of database.monitors and gives u access to the data of the database
-    database.ref().on("child_added",function(childSnapshot){
+    //chilsanapshot,snaphot of database: monitors and gives u access to the data of the database
+    //create firebase event for adding train to the database and a row in html when a user adds an entry
+    database.ref().on("child_added", function (childSnapshot) {
         //console.log(childSnapshot.val());
         var trainNameData = childSnapshot.val().trainName
         //console.log(trainNameData);
@@ -52,23 +54,20 @@ $(document).ready(function () {
         //console.log(trainTime)
         //max moment is equal to either the current time of the day or the first train time. 
         //whichever is further out
-        var maxMoment = moment.max(moment(),trainTime)
-        if (maxMoment === trainTime){
+        var maxMoment = moment.max(moment(), trainTime)
+        if (maxMoment === trainTime) {
             var trainArrival = trainTime.format("hh:mm A")
             //minuteaway=diffrence between the traintime and current time of the day
-            var minutesAway = trainTime.diff(moment(),"minutes")
+            var minutesAway = trainTime.diff(moment(), "minutes")
         } else {
             //differencetime = difference in minutes of how much time has passed from the first train of the day
-            var differenceTime = moment().diff(trainTime,"minutes")
+            var differenceTime = moment().diff(trainTime, "minutes")
             var timeRemainder = differenceTime % frequencyData
-            var minutesAway = frequencyData-timeRemainder
-            var trainArrival = moment().add(minutesAway,"m").format("hh:mm A")
+            var minutesAway = frequencyData - timeRemainder
+            var trainArrival = moment().add(minutesAway, "m").format("hh:mm A")
         }
-        
-        var newRow = "<tr><td>"+trainNameData+"</td><td>"+destinationData+"</td><td>"+trainTime.format("hh:mm A")+"</td><td>"+frequencyData+"</td><td>"+trainArrival+"</td><td>"+minutesAway+"</td></tr>"
+        //add each train's data into the table
+        var newRow = "<tr><td>" + trainNameData + "</td><td>" + destinationData + "</td><td>" + trainTime.format("hh:mm A") + "</td><td>" + frequencyData + "</td><td>" + trainArrival + "</td><td>" + minutesAway + "</td></tr>"
         $(".table").append(newRow);
-
     })
-    
-
 });
